b0ad15f269ef209306a344df1264906654bedd17
don4ara <don4araa@gmail.com>
2025-07-24 20:20:28 +0500

    chore(main): изменение функции запуска приложения

--- src/main.ts
+++ src/main.ts
@@ -6,10 +6,4 @@ async function bootstrap() {
   await app.listen(process.env.PORT ?? 3000);
 }
 
-bootstrap()
-  .then(() => {
-    console.log('Application started');
-  })
-  .catch((err) => {
-    console.error(err);
-  });
+void bootstrap();
