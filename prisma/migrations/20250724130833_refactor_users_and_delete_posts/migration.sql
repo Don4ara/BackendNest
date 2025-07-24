f01403623b65f2691eb0afb7249ca3444ee6aa27
don4ara <don4araa@gmail.com>
2025-07-24 18:11:30 +0500

    feat(migration): удалена таблица постов, изменена таблица Users для авторизации

---
+++ prisma/migrations/20250724130833_refactor_users_and_delete_posts/migration.sql
@@ -0,0 +1,30 @@
+/*
+  Warnings:
+
+  - The primary key for the `users` table will be changed. If it partially fails, the table could be left without primary key constraint.
+  - You are about to drop the column `createdAt` on the `users` table. All the data in the column will be lost.
+  - You are about to drop the column `isStupid` on the `users` table. All the data in the column will be lost.
+  - You are about to drop the column `surname` on the `users` table. All the data in the column will be lost.
+  - You are about to drop the column `updatedAt` on the `users` table. All the data in the column will be lost.
+  - You are about to drop the `posts` table. If the table is not empty, all the data it contains will be lost.
+  - Added the required column `updated_at` to the `users` table without a default value. This is not possible if the table is not empty.
+
+*/
+-- DropForeignKey
+ALTER TABLE "posts" DROP CONSTRAINT "posts_authorId_fkey";
+
+-- AlterTable
+ALTER TABLE "users" DROP CONSTRAINT "users_pkey",
+DROP COLUMN "createdAt",
+DROP COLUMN "isStupid",
+DROP COLUMN "surname",
+DROP COLUMN "updatedAt",
+ADD COLUMN     "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+ADD COLUMN     "updated_at" TIMESTAMP(3) NOT NULL,
+ALTER COLUMN "id" DROP DEFAULT,
+ALTER COLUMN "id" SET DATA TYPE TEXT,
+ADD CONSTRAINT "users_pkey" PRIMARY KEY ("id");
+DROP SEQUENCE "users_id_seq";
+
+-- DropTable
+DROP TABLE "posts";
