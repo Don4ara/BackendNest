dfe0dbae3fb2a25b559acba4e3ef697b504e2e54
don4ara <don4araa@gmail.com>
2025-07-27 18:53:30 +0500

    feat(main): включен CORS с настройками для локальной разработки

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,16 @@ import * as cookieParser from 'cookie-parser';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  app.use(cookieParser())
+  app.use(cookieParser());
+
+  app.enableCors({
+    origin: 'http://localhost:5174', // обязательно с http://
+    credentials: true,
+    methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Accept', 'Authorization', 'X-Requested-With'],
+    exposedHeaders: ['Set-Cookie'],
+  });
+
 
   await app.listen(process.env.PORT ?? 3000);
 }
